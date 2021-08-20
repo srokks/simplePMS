41fe9eda1b3e2a4ae6f293eb81c771b1df6a8212
Srokks <srokks@gmail.com>
2021-08-20 12:25:06 +0200

    First project of searching guest window

--- guest/guestSearchApp.py
+++ guest/guestSearchApp.py
@@ -2,8 +2,9 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-Builder.load_file('searchBar.kv')
 
+Builder.load_file('searchBar.kv')
+Builder.load_file('searchbarLabel.kv')
 
 
 class GuestSearchApp(App):
