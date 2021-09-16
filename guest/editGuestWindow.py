cc3865b856afcd25d2fd76630b8b965f802d8885
srokks <srokks@gmail.com>
2021-09-16 10:57:48 +0200

    added LineEdit

--- guest/editGuestWindow.py
+++ guest/editGuestWindow.py
@@ -21,15 +21,27 @@ from PyQt5.QtWidgets import (
 
 )
 
-
 from PyQt5.QtCore import Qt, QRegExp
+
+
+class LineEdit(QLineEdit):
+    def __init__(self,obligatory=False):
+        super(LineEdit, self).__init__()
+        self.obligatory = obligatory
+        self.setObligatory()
+        self.textChanged.connect(self.setObligatory)
+    def setObligatory(self):
+        if self.obligatory == True:
+            self.setStyleSheet('background-color:rgba(255, 238, 0,90)')
+        else:
+            self.setStyleSheet('background-color:rgb(255, 255, 255)')
 
 class BasicInfo(QWidget):
     def __init__(self):
         super(BasicInfo, self).__init__()
 
         basic_info_lay = QFormLayout()
-        basic_info_lay.setContentsMargins(10,10,10,10)
+        basic_info_lay.setContentsMargins(10, 10, 10, 10)
 
         self.guest_id = QLineEdit()
         self.guest_id.setMaximumWidth(50)
@@ -38,18 +50,18 @@ class BasicInfo(QWidget):
         self.type_cmb = QComboBox()
 
         self.type_cmb.setMaximumWidth(100)
-        self.type_cmb.addItems(['Guest','Company','Agent'])
+        self.type_cmb.addItems(['Guest', 'Company', 'Agent'])
 
         self.gender_cmb = QComboBox()
-        self.gender_cmb.addItems(['Mr.','Mrs.'])
+        self.gender_cmb.addItems(['Mr.', 'Mrs.'])
 
         id_type_lay = QHBoxLayout()
-        id_type_lay.setContentsMargins(0,0,0,0)
+        id_type_lay.setContentsMargins(0, 0, 0, 0)
         id_type_lay.addWidget(self.guest_id)
         id_type_lay.addStretch()
         id_type_lay.addWidget(self.type_cmb)
 
-        self.first_name_le = QLineEdit()
+        self.first_name_le = LineEdit(True)
         self.first_name_le.setMinimumWidth(100)
         self.last_name_le = QLineEdit()
         self.phone_number_le = QLineEdit()
@@ -65,26 +77,28 @@ class BasicInfo(QWidget):
 
         self.id_number_lbl = QLabel("ID number:")
         basic_info_lay.addRow(id_type_lay)
-        basic_info_lay.addRow("Gender:",self.gender_cmb)
-        basic_info_lay.addRow("First Name:",self.first_name_le)
-        basic_info_lay.addRow("Last Name:",self.last_name_le)
-        basic_info_lay.addRow("Phone number:",self.phone_number_le)
-        basic_info_lay.addRow("Mail address:",self.mail_address_le)
-        basic_info_lay.addRow("Address:",self.address_le)
-        basic_info_lay.addRow("Address 2:",self.address2_le)
-        basic_info_lay.addRow("City:",self.city_le)
-        basic_info_lay.addRow("State:",self.state_le)
-        basic_info_lay.addRow("Zip code:",self.zip_code_le)
-        basic_info_lay.addRow("Country:",self.country_le)
-        basic_info_lay.addRow(self.id_number_lbl,self.id_number_le)
+        basic_info_lay.addRow("Gender:", self.gender_cmb)
+        basic_info_lay.addRow("First Name:", self.first_name_le)
+        basic_info_lay.addRow("Last Name:", self.last_name_le)
+        basic_info_lay.addRow("Phone number:", self.phone_number_le)
+        basic_info_lay.addRow("Mail address:", self.mail_address_le)
+        basic_info_lay.addRow("Address:", self.address_le)
+        basic_info_lay.addRow("Address 2:", self.address2_le)
+        basic_info_lay.addRow("City:", self.city_le)
+        basic_info_lay.addRow("State:", self.state_le)
+        basic_info_lay.addRow("Zip code:", self.zip_code_le)
+        basic_info_lay.addRow("Country:", self.country_le)
+        basic_info_lay.addRow(self.id_number_lbl, self.id_number_le)
         self.setLayout(basic_info_lay)
 
         self.type_cmb.currentIndexChanged.connect(self.type_cmb_on_change)
-    def type_cmb_on_change(self,index):
+
+    def type_cmb_on_change(self, index):
         if index == 0:
             self.id_number_lbl.setText('ID number')
-        elif index in [1,2]:
+        elif index in [1, 2]:
             self.id_number_lbl.setText('Company ID')
+
 
 class editGuest(QWidget):
     def __init__(self, gGuest=None):
@@ -96,10 +110,8 @@ class editGuest(QWidget):
         main_layout.addWidget(tab)
         basic_info = BasicInfo()
         basic_info.first_name_le.text()
-        tab.addTab(basic_info,'Basic')
-        tab.addTab(QWidget(),'Family Members')
-
-
+        tab.addTab(basic_info, 'Basic')
+        tab.addTab(QWidget(), 'Family Members')
 
         action_btn_layout = QVBoxLayout()
         action_btn_layout.addWidget(QPushButton("New"))
@@ -107,17 +119,16 @@ class editGuest(QWidget):
         action_btn_layout.addWidget(QPushButton("Close"))
         action_btn_layout.addStretch()
 
-
-
         main_layout.addLayout(action_btn_layout)
         self.setLayout(main_layout)
+
 
 if __name__ == "__main__":
     import sys
 
     app = QApplication(sys.argv)
     MainWindow = editGuest()
-    MainWindow.move(0,0)
-    MainWindow.resize(200,400)
+    MainWindow.move(0, 0)
+    MainWindow.resize(200, 400)
     MainWindow.show()
     sys.exit(app.exec_())
